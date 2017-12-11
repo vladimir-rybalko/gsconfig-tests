186de1d4a4e743bea49a2d145c4a13ab2bf5f35a
Vladimir Rybalko <vladimir.rybalko@gmail.com>
2017-12-11 15:51:08 +0500

    add script delete layers list from file

---
+++ 9 - delete_layers_from_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''Delete layers from list Geoserver WorkSpace'''
+__author__ = "Vladimir Rybalko"
+
+import sys
+from geoserver.catalog import Catalog
+cat = Catalog("http://localhost:8080/geoserver/rest", username="admin", password="geoserver")
+
+if len(sys.argv) > 1:
+    file = open(sys.argv[1])
+    layerArray = []
+    for line in file.read().splitlines():
+        layerArray.append(line)
+    all_layers = cat.get_layers()
+    for l in all_layers:
+        if(l.name in layerArray):
+            cat.delete(l, recurse=True)
+            print u'Слой {0} удален.'.format(l.name)
+else:
+    print u'Необходимо указать аргумент - файл с слоями для удаления.'
